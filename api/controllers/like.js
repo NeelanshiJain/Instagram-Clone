e801ea087be19b0fabcc40dcd7415fd0c3a365a3
Neelanshi <sakshijain3111998@gmail.com>
2024-04-19 10:02:26 +0530

    Implemented client functionality

--- api/controllers/like.js
+++ api/controllers/like.js
@@ -1,14 +1,14 @@
 import { db } from "../connect.js";
 import jwt from "jsonwebtoken";
 
-export const getLikes = (req,res)=>{
+export const getLikes = (req, res) => {
   const q = "SELECT userId FROM likes WHERE postId = ?";
 
   db.query(q, [req.query.postId], (err, data) => {
     if (err) return res.status(500).json(err);
-    return res.status(200).json(data.map(like=>like.userId));
+    return res.status(200).json(data.map((like) => like.userId));
   });
-}
+};
 
 export const addLike = (req, res) => {
   const token = req.cookies.accessToken;
@@ -18,10 +18,7 @@ export const addLike = (req, res) => {
     if (err) return res.status(403).json("Token is not valid!");
 
     const q = "INSERT INTO likes (`userId`,`postId`) VALUES (?)";
-    const values = [
-      userInfo.id,
-      req.body.postId
-    ];
+    const values = [userInfo.id, req.body.postId];
 
     db.query(q, [values], (err, data) => {
       if (err) return res.status(500).json(err);
